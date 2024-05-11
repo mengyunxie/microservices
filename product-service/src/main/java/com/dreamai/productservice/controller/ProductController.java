3bae7c2641d577d1122ed461b63a0d04e38903b9
mengyunxie <my.xiemy@gmail.com>
2024-05-11 12:29:17 -0700

    Distributed tracing with zipkin and micrometer

--- product-service/src/main/java/com/dreamai/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/dreamai/productservice/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.dreamai.productservice.dto.ProductRequest;
 import com.dreamai.productservice.dto.ProductResponse;
 import com.dreamai.productservice.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/product")
 @RequiredArgsConstructor
+@Slf4j
 public class ProductController {
 
     private final ProductService productService;
@@ -19,6 +21,7 @@ public class ProductController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void creatProduct(@RequestBody ProductRequest productRequest) {
+        log.info("new Product {}", productRequest);
         productService.createProduct(productRequest);
     }
 
