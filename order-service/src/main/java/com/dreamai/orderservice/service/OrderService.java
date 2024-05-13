6d713d26c7fa2991bfb62c74fb61b1cb7acfb092
mengyunxie <my.xiemy@gmail.com>
2024-05-13 16:06:21 -0700

    microservices event driven with Kafka

--- order-service/src/main/java/com/dreamai/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/dreamai/orderservice/service/OrderService.java
@@ -3,12 +3,14 @@ package com.dreamai.orderservice.service;
 import com.dreamai.orderservice.dto.InventoryResponse;
 import com.dreamai.orderservice.dto.OrderLineItemsDto;
 import com.dreamai.orderservice.dto.OrderRequest;
+import com.dreamai.orderservice.event.OrderPlacedEvent;
 import com.dreamai.orderservice.model.Order;
 import com.dreamai.orderservice.model.OrderLineItems;
 import com.dreamai.orderservice.repository.OrderRepository;
 import io.micrometer.tracing.Span;
 import io.micrometer.tracing.Tracer;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -25,7 +27,7 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
     private final Tracer tracer;
-
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public String placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -55,6 +57,7 @@ public class OrderService {
 
             if(allProductInStock) {
                 orderRepository.save(order);
+                kafkaTemplate.send("notificationTopic", new OrderPlacedEvent(order.getOrderNumber()));
                 return "Order placed successfully!";
             } else {
                 throw new IllegalArgumentException("Product is not in stock, please try again later.");
